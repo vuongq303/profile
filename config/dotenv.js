2672a3c6cd176e4aa2e4aeb5a82368cacb988bd7
vuongq303 <vuongq303@gmail.com>
2025-03-13 22:51:54 +0700

    a

---
+++ config/dotenv.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+module.exports = {
+  JWT_KEY: process.env.JWT_KEY || "",
+  PORT: process.env.PORT || 3000,
+  HOSTNAME: process.env.HOSTNAME || "localhost",
+  DATABASE_USER: process.env.DATABASE_USER || "root",
+  DATABASE: process.env.DATABASE || "",
+  DATABASE_PORT: process.env.DATABASE_PORT || 3306,
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || "",
+  MONGODB_KEY: process.env.MONGODB_KEY || "",
+  MONGODB_DB: process.env.MONGODB_DB || ""
+};
